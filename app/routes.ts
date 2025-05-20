3aa6e517f1bbddf936e6dde5f2468540f967771a
Jadiel dos Santos <jadieldossantos@ioasys.com.br>
2025-05-20 04:16:38 -0300

    feat: adjusting routes

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,16 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+const routes = [
+  index("routes/home.tsx"),
+  route("login/user", "routes/login.client.tsx"),
+  route("login/photographer", "routes/login.photographer.tsx"),
+  route("register/user", "routes/register.client.tsx"),
+  route("register/photographer", "routes/register.photographer.tsx"),
+  route("dashboard/photographer", "routes/dashboard.photographer._index.tsx"),
+  route("dashboard/user", "routes/dashboard.client._index.tsx"),
+  route("dashboard/photographer/albums/:id", "routes/dashboard.photographer.albums.$albumId.process.tsx"),
+  route("dashboard/photographer/albums/:id/share", "routes/dashboard.photographer.albums.$albumId.share.tsx"),
+  route("access-denied", "routes/access-denied.tsx"),
+];
+
+export default routes satisfies RouteConfig;
